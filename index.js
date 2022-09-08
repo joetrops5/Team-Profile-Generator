a7f5a98f13868f0ebbe30978dd6911f3ea35552b
joetrops5 <jtropss@aol.com>
2022-09-08 08:53:46 -0400

    generated html

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import inquirer from "inquirer";
-import Manager from "./lib/Manager";
-import Engineer from "./lib/Engineer";
-import Intern from "./lib/Intern";
+import Manager from "./lib/Manager.js";
+import Engineer from "./lib/Engineer.js";
+import Intern from "./lib/Intern.js";
+import { generateRoster } from "./src/template-helpers.js";
 
 const start = async () => {
   const team = {
@@ -19,12 +20,12 @@ const start = async () => {
     },
     {
       type: "input",
-      name: "employeeId",
+      name: "id",
       message: "What is your manager's employee ID?",
     },
     {
       type: "input",
-      name: "emailAdress",
+      name: "email",
       message: "What is your manager's email address?",
     },
     {
@@ -36,76 +37,81 @@ const start = async () => {
 
   team.managers.push(new Manager(manager.name, manager.id, manager.email, manager.officeNumber));
 
-  // Menu
-  const answers = await inquirer.prompt([
-    {
-      type: "list",
-      name: "menuOption",
-      message: "What do you want to do next?",
-      choices: ["Add engineer", "Add intern", "Finish"],
-    },
-  ]);
-
-  // NOTE: answers.menuOption is the user's choice
-
-  if (answers.menuOption == "Add engineer") {
-    // Engineer
-    const engineer = await inquirer.prompt([
+  while (true) {
+    // Menu
+    const answers = await inquirer.prompt([
       {
-        type: "input",
-        name: "name",
-        message: "What is the engineer's name?",
-      },
-      {
-        type: "input",
-        name: "employeeId",
-        message: "What is the engineer's employee ID?",
-      },
-      {
-        type: "input",
-        name: "emailAdress",
-        message: "What is the engineer's email address?",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is the engineer's github username?",
+        type: "list",
+        name: "menuOption",
+        message: "What do you want to do next?",
+        choices: ["Add engineer", "Add intern", "Finish"],
       },
     ]);
 
-    team.engineers.push(new Engineer(engineer.name, engineer.id, engineer.email, engineer.github));
-  }
+    // NOTE: answers.menuOption is the user's choice
 
-  if (answers.menuOption == "Add intern") {
-    // Intern
-    const intern = await inquirer.prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is the intern's name?",
-      },
-      {
-        type: "input",
-        name: "employeeId",
-        message: "What is the intern's employee ID?",
-      },
-      {
-        type: "input",
-        name: "emailAdress",
-        message: "What is the intern's email address?",
-      },
-      {
-        type: "input",
-        name: "school",
-        message: "What is the intern's school?",
-      },
-    ]);
+    if (answers.menuOption == "Add engineer") {
+      // Engineer
+      const engineer = await inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the engineer's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the engineer's employee ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the engineer's email address?",
+        },
+        {
+          type: "input",
+          name: "github",
+          message: "What is the engineer's github username?",
+        },
+      ]);
 
-    team.interns.push(new Intern(intern.name, intern.id, intern.email, intern.school));
-  }
+      team.engineers.push(
+        new Engineer(engineer.name, engineer.id, engineer.email, engineer.github)
+      );
+    }
 
-  if (answers.menuOption == "Finish") {
-    
+    if (answers.menuOption == "Add intern") {
+      // Intern
+      const intern = await inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "What is the intern's name?",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "What is the intern's employee ID?",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the intern's email address?",
+        },
+        {
+          type: "input",
+          name: "school",
+          message: "What is the intern's school?",
+        },
+      ]);
+
+      team.interns.push(new Intern(intern.name, intern.id, intern.email, intern.school));
+    }
+
+    if (answers.menuOption == "Finish") {
+      console.log(generateRoster(team));
+      process.exit();
+    }
   }
 };
 
--- src/template-helpers.js
+++ src/template-helpers.js
@@ -1,8 +1,89 @@
+function getIcon(role) {
+  if (role == "Manager") {
+    return "☕";
+  }
+  if (role == "Engineer") {
+    return "☕";
+  }
+  if (role == "Intern") {
+    return "☕";
+  }
+}
+
 /**
  * Returns a string containing HTML generated from the `team` parameter.
  */
 export function generateRoster(team) {
   return `
-        
-    `;
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="style.css" />
+        <title>Team Roster</title>
+      </head>
+      <body>
+        <header>My Team</header>
+        <main>
+
+          ${team.managers
+            .map(
+              (manager) => `
+              <div class="card">
+                <header>
+                  ${manager.getName()}
+                  <div>${getIcon(manager.getRole())} ${manager.getRole()}</div>
+                </header>
+                <main>
+                  <div>ID: ${manager.getId()}</div>
+                  <div>Email: ${manager.getEmail()}</div>
+                  <div>Office number: ${manager.getOfficeNumber()}</div>
+                </main>
+              </div>
+            `
+            )
+            .join("")}
+
+          ${team.engineers
+            .map(
+              (engineer) => `
+              <div class="card">
+                <header>
+                  ${engineer.getName()}
+                  <div>${getIcon(engineer.getRole())} ${engineer.getRole()}</div>
+                </header>
+                <main>
+                  <div>ID: ${engineer.getId()}</div>
+                  <div>Email: ${engineer.getEmail()}</div>
+                  <div>GitHub: ${engineer.getGithub()}</div>
+                </main>
+              </div>
+            `
+            )
+            .join("")}
+
+          ${team.interns
+            .map(
+              (intern) => `
+              <div class="card">
+                <header>
+                  ${intern.getName()}
+                  <div>${getIcon(intern.getRole())} ${intern.getRole()}</div>
+                </header>
+                <main>
+                  <div>ID: ${intern.getId()}</div>
+                  <div>Email: ${intern.getEmail()}</div>
+                  <div>School: ${intern.getSchool()}</div>
+                </main>
+              </div>
+            `
+            )
+            .join("")}
+          
+        </main>
+      </body>
+    </html>
+  `;
 }
